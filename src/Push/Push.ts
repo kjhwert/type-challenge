6d2a4d90ba0bbc22f0feee94eab94b425939a38c
kjhwert <kjhwert11@naver.com>
2023-10-13 13:55:12 +0900

    feat: Unshift

--- src/Push/Push.ts
+++ src/Push/Push.ts
@@ -1,3 +1,7 @@
 type Push<Arr extends unknown[], T> = [...Arr, T];
 
-type Result = Push<[1, 2], '3'>
+type PushResult = Push<[1, 2], '3'>
+
+type Unshift<Arr extends unknown[], T> = [T, ...Arr];
+
+type UnshiftResult = Unshift<[1, 2], 0>
